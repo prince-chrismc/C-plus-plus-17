62b1775fa64a91cc9dbf5933a7e43612663e412a
Chris Mc <prince.chrismc@gmail.com>
2017-09-28 22:50:47 -0400

    successfully compile and ran c++1z code

--- C++1z/C++1z.cpp
+++ C++1z/C++1z.cpp
@@ -6,10 +6,28 @@
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
+/*
+ * Special Thanks to https://github.com/AnthonyCalandra/modern-cpp-features/blob/master/CPP17.md
+ * for providing examples and inspiration about what to demo and try!
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// Folding Expression
+template<typename... Args>
+bool logicalAnd(Args... args) {
+    // Binary folding.
+    return (true && ... && args);
+}
+
 int main(void) {
 	puts("Hello World!!!");
+
+	bool b = true;
+	bool& b2 = b;
+	logicalAnd(b, b2, true); // == true
+
+
 	return EXIT_SUCCESS;
 }
---
+++ C++1z/compile.sh
@@ -0,0 +1,2 @@
+g++-7 -std=c++1z C++1z.cpp -o C++1z
+
